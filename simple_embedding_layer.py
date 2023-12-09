3a47b632576ff5da83cafcd41f3be66244868125
Monish Reddy Kotturu <monishkotturu@gmail.com>
2023-12-08 23:20:14 -0500

    fixed dimension issues in train and incorrect function names
    embedding layer still has bugs

--- simple_embedding_layer.py
+++ simple_embedding_layer.py
@@ -7,7 +7,7 @@ class SimpleEmbeddingLayer(Layer):
         self.embedding_dim = embedding_dim
         self.weights = np.random.rand(num_embeddings, embedding_dim) - 0.5
 
-    def backward(self, output_error, learning_rate):
+    def backward_propagation(self, output_error, learning_rate):
         # Derivative of the error with respect to the weights
         weights_error = np.zeros_like(self.weights)
         np.add.at(weights_error, self.input, output_error)
@@ -20,7 +20,7 @@ class SimpleEmbeddingLayer(Layer):
 
         return input_error
 
-    def forward(self, input_data):
+    def forward_propagation(self, input_data):
         # input_data should be an array of indices
         self.input = input_data
         self.output = self.weights[input_data]
